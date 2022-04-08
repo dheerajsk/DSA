028eeffc02cb4d734649a293a5e15370ebef8b1a
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-04-08 22:51:08 +0530

    minIncreament

---
+++ sortingPractices/minimumIncreament.js
@@ -0,0 +1,22 @@
+
+
+
+function minIncreament(array){
+    
+    array.sort((a,b)=> a-b);
+    let counter=0;
+    for(let i=1; i<array.length; i++){
+       if(array[i]<=array[i-1]){
+           const temp = array[i];
+           array[i] = array[i-1]+1;
+           counter+=array[i]-temp;
+       }
+    }
+    console.log(array);
+    console.log(counter);
+}
+
+
+const array = [3,7,1,2,1,4]
+// o/p => 4   => [3,7,5,2,1,4]
+minIncreament(array);
